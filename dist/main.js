ae86b9b304f67aa138b03576f6c8f6010d749526
Wizary <wizaryn@gmail.com>
2018-07-24 09:10:25 +0100

    styling

--- dist/main.js
+++ dist/main.js
@@ -4423,7 +4423,7 @@ function () {
         return;
       }
 
-      this.target.querySelector('.quiz__counter').innerHTML = counter + ' / ' + this.questionsCount;
+      this.target.querySelector('.quiz__counter').innerHTML = 'Вопросы: ' + counter + ' / ' + this.questionsCount;
     }
   }, {
     key: "setQuestion",
@@ -4511,34 +4511,64 @@ var src_prepareForms = function prepareForms() {
 
 var src_createQuiz = function createQuiz() {
   var testQuiz = new Quiz('#test-quiz', 't-quiz', 'Тестовый Квиз', [{
-    question: 'Какой стиль вам нравится?',
+    question: 'Что посмотреть?',
     answers: [{
-      text: 'Хайтек',
-      image: 'test1.jpg'
+      text: 'Спорт',
+      image: 'q1-1.jpg'
     }, {
-      text: 'Второе название стиля',
-      image: 'test2.jpg'
+      text: 'Кино / Сериалы',
+      image: 'q1-2.jpg'
     }, {
-      text: 'Ещё какое-то название стиля',
-      image: 'test3.jpg'
+      text: 'Мультфильмы',
+      image: 'q1-3.jpg'
+    }, {
+      text: 'Научно-познавательные фильмы',
+      image: 'q1-4.jpg'
     }]
   }, {
-    question: 'Прив чё дел?',
+    question: 'Сколько членов семьи??',
     answers: [{
-      text: 'Да',
-      image: 'test1.jpg'
+      text: 'Я и кот',
+      image: 'q2-1.jpg'
     }, {
-      text: 'Нет',
-      image: 'test2.jpg'
+      text: '2',
+      image: 'q2-2.jpg'
+    }, {
+      text: 'Папа, мама, я',
+      image: 'q2-3.jpg'
+    }, {
+      text: 'Нас миллионы',
+      image: 'q2-4.jpg'
     }]
   }, {
-    question: 'Ещё один вопрос?',
+    question: 'Где хотите (смотреть ТВ)?',
     answers: [{
-      text: 'Да',
-      image: 'test1.jpg'
+      text: 'В спальне',
+      image: 'q3-1.jpg'
     }, {
-      text: 'Нет',
-      image: 'test2.jpg'
+      text: 'В гостинной',
+      image: 'q3-2.jpg'
+    }, {
+      text: 'На кухне',
+      image: 'q3-3.jpg'
+    }, {
+      text: 'Везде хочу!',
+      image: 'q3-4.jpg'
+    }]
+  }, {
+    question: 'Какой цвет дарит скидки?',
+    answers: [{
+      text: 'Этот',
+      image: 'q4-1.jpg'
+    }, {
+      text: 'Этот',
+      image: 'q4-2.jpg'
+    }, {
+      text: 'Вот этот',
+      image: 'q4-3.jpg'
+    }, {
+      text: 'ЗЕЛЁНЫЙ',
+      image: 'q4-4.jpg'
     }]
   }]);
 };
