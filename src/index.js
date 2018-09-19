84c87b148050e4af8094f5d96f18251be7d8e6a1
Wizary <wizaryn@gmail.com>
2018-09-19 17:39:21 +0300

    parrot final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import style from "./_scss/main.scss";
 import AOS from 'aos';
+import Inputmask from "inputmask";
 import { ready } from './modules/utility';
 import Modal from './modules/modal';
 import Form from './modules/form';
@@ -11,12 +12,36 @@ let addModals = () => {
 }
 
 let prepareForms = () => {
-    const testForm = new Form(
-        document.querySelector('.test-form'),
-        'test.php', 
+    // const testForm = new Form(
+    //     document.querySelector('.test-form'),
+    //     'test.php', 
+    //     undefined, 
+    //     {wrapper: '.field', error: '.error-message', fieldWarningClass: 'is-danger'}
+    // )
+    const loginForm = new Form(
+        document.querySelector('.login-form'),
+        undefined, 
         undefined, 
         {wrapper: '.field', error: '.error-message', fieldWarningClass: 'is-danger'}
     )
+    const phoneForm = new Form(
+        document.querySelector('.phone-form'),
+        undefined, 
+        undefined, 
+        {wrapper: '.field', error: '.error-message', fieldWarningClass: 'is-danger'}
+    )
+    const ыьыАщкь = new Form(
+        document.querySelector('.sms-form'),
+        undefined, 
+        undefined, 
+        {wrapper: '.field', error: '.error-message', fieldWarningClass: 'is-danger'}
+    )
+    Inputmask().mask(document.querySelectorAll("input"));
+    document.querySelector('.js-reveal-sms').addEventListener('click', ()=>{
+        document.querySelector('.phone-form').classList.add('is-hidden')
+        document.querySelector('.sms-form').classList.remove('is-hidden')
+    })
 }
 
-ready([addModals, prepareForms])
+// ready([addModals, prepareForms])
+ready([prepareForms])
--- src/modules/form/index.js
+++ src/modules/form/index.js
@@ -10,10 +10,12 @@
 */
 
 export default class Form {
-    constructor(form, url, checkModel = {email: /.+\@.+\..+/}, inputDom = {}) {
+    constructor(form, url, checkModel = {email: /.+\@.+\..+/, password: /\S+/, username: /\S+/}, inputDom = {}) {
         this.form = form;
+        this.url = url;
         this.checkModel = checkModel;
         this.inputDom = inputDom;
+        this.data = {}
         this.button = this.form.querySelector('.js-submit-form')
         this.button.addEventListener('click', event => {
             event.preventDefault();
@@ -21,25 +23,43 @@ export default class Form {
         })
     }
 
-    collect() {
+    collect() {        
         this.inputs = Array.from(this.form.elements).filter(el => el.tagName !== 'BUTTON');
-        this.check()        
+        if(this.check()) {
+            this.inputs.forEach(el => {
+                this.data[el.name] = el.value
+            })
+            this.send()      
+        }  
     }
 
     check() {
+        let passFlag = true;
         Array.from(document.querySelectorAll(this.inputDom.error)).forEach(error=>error.classList.add('is-invisible'))
         this.inputs.forEach(el => {
             el.classList.remove(this.inputDom.fieldWarningClass)
             if(this.checkModel[el.name]) {
                 if(el.value.search(this.checkModel[el.name]) < 0) {
                     this.printError(el)
+                    passFlag = false;
                 }                
             }            
         })
+        return passFlag;
     }
 
     send() {
-        
+        if(this.url) {
+            
+        } else {
+            let params = {
+                method: 'POST',
+                body: JSON.stringify(this.data) 
+            }
+            fetch(this.form.dataset.send, params)
+                .then(response => response.json())
+                .then(json => console.log(json))
+        }
     }
 
     printError(el) {        
